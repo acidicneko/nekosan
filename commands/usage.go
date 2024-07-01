4e8d7b31ca98851d353e8df8b66c4aa5d757db5a
acidicneko <ayush7788899@gmail.com>
2024-07-01 10:28:16 +0530

    add: embed in usage command

--- commands/usage.go
+++ commands/usage.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -10,13 +11,44 @@ var usage = cmdlet{
 	Name:  "usage",
 	Usage: "print usage information about given command",
 	Run: func(args []string, bot *discordgo.Session, event *discordgo.MessageCreate) {
-		requiredCmd, ok := cmdMap[args[0]]
-		if !ok {
-			bot.ChannelMessageSend(event.ChannelID, fmt.Sprintf("usage: %s: no such command", args[0]))
+		if len(args) < 1 {
+			bot.ChannelMessageSend(event.ChannelID, "`usage: no command specified`")
 			return
 		}
-		msg := fmt.Sprintf("Command name: `%s`\nUsage: `%s`", requiredCmd.Name, requiredCmd.Usage)
-		_, err := bot.ChannelMessageSend(event.ChannelID, msg)
+		requiredCmd, ok := cmdMap[args[0]]
+		if !ok {
+			bot.ChannelMessageSend(event.ChannelID, fmt.Sprintf("`usage: %s: no such command`", args[0]))
+			return
+		}
+		embed := &discordgo.MessageEmbed{
+			Title: "Command usage info",
+			Author: &discordgo.MessageEmbedAuthor{
+				Name:    event.Author.Username,
+				IconURL: event.Author.AvatarURL(""),
+			},
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "Command Name",
+					Value:  requiredCmd.Name,
+					Inline: false,
+				},
+				{
+					Name:   "Usage",
+					Value:  requiredCmd.Usage,
+					Inline: false,
+				},
+			},
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL: "https://raw.githubusercontent.com/acidicneko/nekosan/main/assets/cat.png",
+			},
+			Color:     0xed2939,                        // Red
+			Timestamp: time.Now().Format(time.RFC3339), // Discord wants ISO8601; RFC3339 is an extension of ISO8601 and should be completely compatible.
+			Footer: &discordgo.MessageEmbedFooter{
+				Text:    "NekoSan",
+				IconURL: bot.State.User.AvatarURL(""),
+			},
+		}
+		_, err := bot.ChannelMessageSendEmbed(event.ChannelID, embed)
 		if err != nil {
 			fmt.Println("Error while sending messages!")
 		}
